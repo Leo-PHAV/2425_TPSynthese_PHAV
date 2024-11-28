8d4cfeb587b1130a3df343c8887843586f095c96
Leo-PHAV <leo.phav@ensea.fr>
2024-11-28 11:38:17 +0100

    Q4

---
+++ constantHeader.h
@@ -0,0 +1,10 @@
+#define BUFSIZE 128
+
+const char *welcomeMessage = "Welcone in ENSEA's Shell.\n";
+const char *quitMessage = "To quit, type 'exit'.\n";
+const char *prompt = "enseash % ";
+const char *exitCommand = "exit";
+const char *exitMessage = "See you next time!\n";
+
+
+char buffer[BUFSIZE];
--- enseash.c
+++ enseash.c
@@ -1,4 +1,53 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
+#include <string.h>
+#include <sys/types.h>
+#include <sys/wait.h>
 
+#include "constantHeader.h"
+
+
+void displayWelcomeMessage() {
+    write(STDOUT_FILENO, welcomeMessage, strlen(welcomeMessage));
+    write(STDOUT_FILENO, quitMessage, strlen(quitMessage));
+}
+
+void readCommand(){
+    while (1) {
+        write(STDOUT_FILENO, prompt, strlen(prompt));
+
+        ssize_t bytesRead = read(STDIN_FILENO, buffer, sizeof(buffer) - 1);
+
+         if (bytesRead == 0) {
+            write(STDOUT_FILENO, exitMessage, strlen(exitMessage));
+            break;
+        }
+
+        if (bytesRead > 0) {
+            buffer[bytesRead - 1] = '\0'; 
+            if (strcmp(buffer, exitCommand) == 0) {
+                write(STDOUT_FILENO, exitMessage, strlen(exitMessage));
+                break;
+            }
+        }
+
+        pid_t pid = fork();
+        if (pid == 0) {
+            execlp(buffer, buffer, NULL);
+            perror("Unknown command");
+            exit(EXIT_FAILURE);
+        } else if (pid > 0) {
+            wait(NULL);
+        }
+    }
+}   
+
+
+
+
+int main() {
+    //displayWelcomeMessage();
+    readCommand();
+    return 0;
+}
